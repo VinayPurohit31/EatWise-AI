22e9bfda3ec665925ce7a4d41fabeb93fb826a79
VinayPurohit31 <vinaypurohit31@gmail.com>
2025-11-23 12:10:17 +0530

    the endpoint is giving nutri of the product barcode and rating it

--- src/main/java/com/nutriscan/eatwise_ai/config/WebClientConfig.java
+++ src/main/java/com/nutriscan/eatwise_ai/config/WebClientConfig.java
@@ -1,11 +1,18 @@
 package com.nutriscan.eatwise_ai.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+    // Provide a builder bean so @Autowired WebClient.Builder is always available
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
 
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
